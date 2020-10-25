343bc3c75671dbfb2d5c6533931ae9a9d028a7f1
jindalakhil <akhiljindal222@gmail.com>
2020-10-25 23:13:00 +0530

    [Akhil]Addedd : Ride class to calculate Fare For Multiple Rides

---
+++ src/main/java/com/capg/cabservice/Ride.java
@@ -0,0 +1,11 @@
+package com.capg.cabservice;
+
+public class Ride {
+	public double distnce;
+	public int time;
+
+	public Ride(double distance, int time) {
+		this.distnce = distance;
+		this.time = time;
+	}
+}
